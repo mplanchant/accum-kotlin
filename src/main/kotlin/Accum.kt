5e0a117b6d02b236890debc67db888e4ee26ea4b
Matthew Planchant <matt@logiccache.co.uk>
2018-11-30 11:26:46 +0000

    Tweak.

--- src/main/kotlin/Accum.kt
+++ src/main/kotlin/Accum.kt
@@ -2,7 +2,7 @@ package uk.co.logiccache.accum
 
 fun accum(characters: String): String {
     if (characters.isBlank()) return ""
-    return characters.mapIndexed { index, it -> repeatChar(index + 1, it) }.joinToString(separator = "-")
+    return characters.mapIndexed { index, c -> repeatChar(index + 1, c) }.joinToString(separator = "-")
 }
 
 private fun repeatChar(index: Int, character: Char): String {
